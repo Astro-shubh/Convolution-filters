acf702ae64fc39a5ea8233b5dd5af603ca2a297c
Astro-shubh <shubh809singh@gmail.com>
2024-06-24 12:58:21 +0100

    Added final parts of the code and made modifications to correct the filter shape.

--- fresnel_test.cpp
+++ fresnel_test.cpp
@@ -4,32 +4,69 @@
 #include "source/Fresnel_integrals_PRESTO.h"
 #include "source/Fresnel_for_vector.h"
 #include "source/template_generator.h"
+#include "source/padding_and_fft.h"
 
 using namespace std;
+
+
 int main(){
+	struct sample{
+		float real;
+		float imag;
+	};
 	double x=10.0,y,z;
 	double *SS;
 	double *ZZ;
 	vector<double> Res1;
 	Res1=fresnl(x);
-	fftw_complex fft[30];
+	fftw_complex fft[50];
 //	cout << Res1[0] << endl;
 //	cout << Res1[1] << endl;
 	int i, size=100;
 	vector<double> Inpt;
 	vector<double> Res;
+	vector<double> Final_filtr, Final_filti;
 	for(i=0; i<size; i++){
-		Inpt.push_back(i*1.0);
+		Inpt.push_back(i*0.1);
 	}
 	Fresnel_for_vector Fresnel(Inpt);
 	 Fresnel.compute_fresnel();
-	Res=Fresnel.FrS;
-//	for(i=0;i<size;i++){cout << Res[i] << endl;}
-	double drift=30.0;
-	template_generator template1(drift);
-	template1.template_calc();
-	ofstream outfile("results.txt");
-	for(i=0; i<template1.q.size(); i++){ outfile << to_string(template1.q[i])+" "+to_string(template1.final_tempr[i])+" "+to_string(template1.final_tempi[i])+" "+to_string(sqrt(pow(template1.final_tempi[i],2)+pow(template1.final_tempr[i],2.0)))+" "+to_string(atan(template1.final_tempi[i]/template1.final_tempr[i])) << endl;}
-	for(i=0; i<template1.q.size(); i++){ cout << template1.final_tempr[i] << endl;}
-}
+	Res=Fresnel.FrC;
 
+	ofstream outfile("FrC_100.txt");
+	for(i=0; i<Res.size(); i++){	
+		outfile << Res[i] << endl;
+	}
+	outfile.close();
+	double width=30.0;
+	vector<double> q, temp_real, temp_imag, real_padding, imag_padding;
+	ofstream outfile1("Filter_30.txt");
+	template_generator template1(width);
+	template1.template_calc();	
+	temp_real=template1.final_tempr;
+	temp_imag=template1.final_tempi;
+	q=template1.q;
+	for(i=0;i<temp_real.size(); i++){
+		outfile1 << to_string(q[i])+" "+to_string(temp_real[i])+" "+to_string(temp_imag[i]) << endl;
+	}
+	outfile1.close();
+	padding_and_fft filter_final(temp_real, temp_imag);
+	filter_final.do_padding();
+	real_padding=filter_final.in_real;
+	imag_padding=filter_final.in_imag;
+	ofstream outfile2("Filter_padding_30.txt");
+	for(i=0;i<real_padding.size(); i++){
+                outfile2 << to_string(q[i])+" "+to_string(real_padding[i])+" "+to_string(imag_padding[i]) << endl;
+        }
+	outfile2.close();
+
+	filter_final.do_fft();
+	ofstream outfile3("Filter_padding_fft_30.txt");
+        for(i=0;i<filter_final.out_real.size(); i++){
+                outfile3 << to_string(q[i])+" "+to_string(filter_final.out_real[i])+" "+to_string(filter_final.out_imag[i]) << endl;
+        }
+        outfile3.close();
+
+
+	
+}
--- source/template_generator.cpp
+++ source/template_generator.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 template_generator::template_generator(double drift1){drift=drift1;}
 void template_generator::width_array(){for(j=-1*drift ;j < drift+1 ; j++){q.push_back(j);}}
-void template_generator::left_edge_array(){for(j=-1*drift;j<drift; j++){Z.push_back(j+drift/2.0);}}
-void template_generator::right_edge_array(){for(j=-1*drift;j<drift; j++){Y.push_back(j-drift/2.0);}}
+void template_generator::left_edge_array(){for(j=-1*drift;j<drift; j++){Z.push_back(sqrt(2.0/drift)*(j+drift/2.0));}}
+void template_generator::right_edge_array(){for(j=-1*drift;j<drift; j++){Y.push_back(sqrt(2.0/drift)*(j-drift/2.0));}}
 void template_generator::compute_SC(){
 	Fresnel_for_vector Fresnel(Y);
 	Fresnel.compute_fresnel();
@@ -16,12 +16,12 @@ void template_generator::compute_SC(){
 	Cz=Fresnel1.FrC;
 }
 void template_generator::template_calc1(){
-	for(i=0;i<Y.size();i++){temp1.push_back(pi*Y[i]*Y[i]/drift);}
+	for(i=0;i<Y.size();i++){temp1.push_back(pi*(q[i]-drift/2.0)*(q[i]-drift/2.0)/drift);}
 }
 
 void template_generator::template_calc2(){
-        for(i=0;i<Y.size();i++){final_tempr.push_back(cos(temp1[i])*(Sz[i]-Sy[i]));}
-	for(i=0;i<Y.size();i++){final_tempi.push_back(sin(temp1[i])*(Cz[i]-Cy[i]));}
+        for(i=0;i<Y.size();i++){final_tempr.push_back((1.0/sqrt(2.0*drift))*(cos(temp1[i])*(Sz[i]-Sy[i]) - sin(temp1[i])*(Cz[i]-Cy[i])));}
+	for(i=0;i<Y.size();i++){final_tempi.push_back((1.0/sqrt(2.0*drift))*(sin(temp1[i])*(Sz[i]-Sy[i]) + cos(temp1[i])*(Cz[i]-Cy[i])));}
 }
 void template_generator::template_calc(){
 	width_array();
